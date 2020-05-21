e45e614738da119c0dd5dc15aaec1dd29960b40f
hrcosko <hrcosko@gmail.com>
2020-05-21 11:28:32 +0200

    punjenje_dohvacanje

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
 using System.Text;
 using EventStore.ClientAPI;
+using EventStore.ClientAPI.Exceptions;
+using EventStore.ClientAPI.Projections;
+using EventStore.ClientAPI.SystemData;
 using Newtonsoft.Json;
+using ConsoleLogger = EventStore.ClientAPI.Common.Log.ConsoleLogger;
 
 
 
@@ -10,33 +18,175 @@ namespace EventStoreNBP
 {
     class Program
     {
-        private static EventData CreateSample(int i)
+        public static class Globals
         {
-            var sampleObject = new { novi_zapis = i };
-            var data = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(sampleObject));
-            var metadata = Encoding.UTF8.GetBytes("{}");
-            var eventPayload = new EventData(Guid.NewGuid(), "event-type", true, data, metadata);
-            return eventPayload;
+            public const String filePath = "./samples/shoppingCart.json";
+            public const String streamName = "kosaricaHrvoje";
+
+            public static readonly UserCredentials AdminCredentials = new UserCredentials("admin", "changeit");
+
+            public static readonly ProjectionsManager Projection = new ProjectionsManager(new ConsoleLogger(),
+                new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2113), TimeSpan.FromMilliseconds(5000));
         }
 
-        public static void Main()
+        private static IEventStoreConnection CreateConnection()
         {
             var conn = EventStoreConnection.Create(new Uri("tcp://admin:changeit@localhost:1113"));
             conn.ConnectAsync().Wait();
+            return conn;
+        }
 
-            using (var transaction = conn.StartTransactionAsync("newstream", ExpectedVersion.Any).Result)
+        private static List<EventData> ProcessEvents(String filePath)
+        {
+            var events = JsonConvert.DeserializeObject<dynamic>(File.ReadAllText(filePath));
+            var eventData = new List<EventData>();
+            foreach (var @event in events)
             {
-                transaction.WriteAsync(CreateSample(1)).Wait();
-                transaction.WriteAsync(CreateSample(2)).Wait();
-                conn.AppendToStreamAsync("newstream", ExpectedVersion.Any, CreateSample(3)).Wait();
-                transaction.WriteAsync(CreateSample(4)).Wait();
-                transaction.WriteAsync(CreateSample(5)).Wait();
-                transaction.CommitAsync().Wait();
+                var id = @event.eventId.ToString();
+                var eventType = @event.eventType.ToString();
+                eventData.Add(new EventData(Guid.Parse(id), eventType, true,
+                    Encoding.UTF8.GetBytes(@event.data.ToString()), null));
             }
+
+            return eventData;
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                switch (args[0])
+                {
+                    case "napuniStream":
+                        napuniStream();
+                        break;
+                    case "procitajEvente":
+                        procitajEvente();
+                        break;
+                    case "step3":
+                        Step3();
+                        break;
+                    case "step3update":
+                        Step3Update();
+                        break;
+                    case "step3options":
+                        Step3ProjectionOptions();
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.GetBaseException().Message);
+            }
+
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+        }
+
+        static void napuniStream()
+        {
+            var conn = CreateConnection();
+            var streamName = Globals.streamName;
+            var step1EventData = ProcessEvents(Globals.filePath);
+            var eventData = step1EventData.ToArray();
+
+            conn.AppendToStreamAsync(streamName, ExpectedVersion.Any, eventData).Wait();
+            Console.WriteLine($"Uploadano {step1EventData.Count} eventa u '{Globals.streamName}'");
+        }
+
+        static void procitajEvente()
+        {
+            var conn = CreateConnection();
+            var streamName = Globals.streamName;
 
             var readEvents = conn.ReadStreamEventsForwardAsync(streamName, 0, 10, true).Result;
             foreach (var evt in readEvents.Events)
                 Console.WriteLine(Encoding.UTF8.GetString(evt.Event.Data));
+
+            var readResult = conn.ReadEventAsync(streamName, 0, true).Result;
+            Console.WriteLine(Encoding.UTF8.GetString(readResult.Event.Value.Event.Data));
         }
+
+        static void Step3()
+        {
+            var conn = CreateConnection();
+            var adminCredentials = Globals.AdminCredentials;
+            var projection = Globals.Projection;
+
+            string countItemsProjection = @"
+                    fromAll().when({
+                    $init: function(){
+                        return {
+                            count: 0
+                        }
+                    },
+                    ItemAdded: function(s,e){
+                        if(e.body.Description.indexOf('Xbox One S') >= 0){
+                            s.count += 1;
+                        }
+                    }
+        })
+    ";
+            projection.CreateContinuousAsync("xbox-one-s-counter", countItemsProjection, adminCredentials).Wait();
+
+            var projectionState = projection.GetStateAsync("xbox-one-s-counter", Globals.AdminCredentials);
+            Console.WriteLine(projectionState.Result);
+        }
+
+        static void Step3Update()
+        {
+            var conn = CreateConnection();
+            var projection = Globals.Projection;
+            var adminCredentials = Globals.AdminCredentials;
+
+            string countItemsProjectionUpdate = @"
+                    fromAll()
+                        .when({
+                            $init: function(){
+                                return {
+                                    count: 0
+                                }
+                            },
+                        ItemAdded: function(s,e){
+                            if(e.body.Description.indexOf('Xbox One S') >= 0){
+                                s.count += 1;
+                            }
+                        }
+                    }).outputState()";
+
+            projection.UpdateQueryAsync("xbox-one-s-counter", countItemsProjectionUpdate, adminCredentials).Wait();
+            projection.ResetAsync("xbox-one-s-counter", adminCredentials).Wait();
+
+            var readEvents = conn.ReadStreamEventsForwardAsync("$projections-xbox-one-s-counter-result", 0, 10, true)
+                .Result;
+            foreach (var evt in readEvents.Events)
+                Console.WriteLine(Encoding.UTF8.GetString(evt.Event.Data));
+
+            string optionsProjectionOptionsUpdate = @"
+                    options({
+                      resultStreamName: 'xboxes'
+                                })
+                                fromAll()
+                                    .when({
+                                    $init: function(){
+                                        return {
+                                            count: 0
+                                        }
+                                    },
+                                    ItemAdded: function(s,e){
+                                        if(e.body.Description.indexOf('Xbox One S') >= 0){
+                                            s.count += 1;
+                                        }
+                                    }
+                                }).outputState()";
+
+            projection.UpdateQueryAsync("xbox-one-s-counter", optionsProjectionOptionsUpdate, adminCredentials).Wait();
+
+            readEvents = conn.ReadStreamEventsForwardAsync("xboxes", 0, 10, true).Result;
+            foreach (var evt in readEvents.Events)
+                Console.WriteLine(Encoding.UTF8.GetString(evt.Event.Data));
+        }
+
+
     }
 }
